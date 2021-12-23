d6736cabb33355a249bbd0682d5ff82aaebe1969
aashna-prakash <aashna.prakash@sap.com>
2021-12-24 00:17:41 +0530

    module3

--- src/main/java/com/pluralsight/blog/model/Post.java
+++ src/main/java/com/pluralsight/blog/model/Post.java
@@ -18,6 +18,9 @@ public class Post {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
+
+    @ManyToOne (fetch = FetchType.EAGER)
+    private Author author;
     @Column(length=1000000)
     @Lob
     private String body;
@@ -33,6 +36,14 @@ public class Post {
         this();
         this.title = title;
         this.body = body;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
     }
 
     public Long getId() {
